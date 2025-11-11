8c0631fe8ed9afc279ce8462b7a2f04bbfa0c178
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 22:52:15 +0100

    I_Truco

---
+++ Java/src/Codewars2024/I_Truco.java
@@ -0,0 +1,49 @@
+package Codewars2024;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class I_Truco {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        int t = in.nextInt();
+
+        int[] results = new int[t];
+
+        for(int j = 0; j < t; ++j)
+        {
+            int n = in.nextInt();
+            in.nextLine();
+
+            int result = 0;
+
+            for(int i = 0; i < n; ++ i)
+            {
+                var strs = in.nextLine().split(" ");
+                int a = Integer.parseInt(strs[0]);
+                int b = Integer.parseInt(strs[2]);
+
+                var sign = strs[1];
+
+                int r = 0;
+                if(sign.equals("+"))
+                    r = a + b;
+                else if(sign.equals("-"))
+                    r = a - b;
+                else
+                    r = a * b;
+
+                result += Math.max(1, r);
+            }
+
+            results[j] = result;
+        }
+
+        for(int j = 0; j < t; ++j)
+            System.out.println(results[j]);
+    }
+}
