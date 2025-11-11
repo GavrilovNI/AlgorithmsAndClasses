ac9f3df7bfb10fba21beafebea9bebfd16919c9e
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 20:42:21 +0100

    R_Subarray

---
+++ Java/src/Codewars2024/R_Subarray.java
@@ -0,0 +1,45 @@
+package Codewars2024;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class R_Subarray {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int sum = 0;
+        int[] values = new int[n];
+
+        int l = 0;
+        int result = 0;
+
+        for(int i = 0; i < n; ++i)
+        {
+            var value = in.nextInt();
+            values[i] = value;
+
+            sum += value;
+            while(sum > k && l <= i)
+            {
+                sum -= values[l];
+                ++l;
+            }
+
+            if(sum <= k)
+            {
+                var length = i - l + 1;
+                if(length > result)
+                    result = length;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
