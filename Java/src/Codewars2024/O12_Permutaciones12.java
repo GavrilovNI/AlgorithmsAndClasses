08288d0a6fd80826ca7b5b48d4446500bef36fb4
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 21:49:54 +0100

    O12_Permutaciones12

---
+++ Java/src/Codewars2024/O12_Permutaciones12.java
@@ -0,0 +1,45 @@
+package Codewars2024;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class O12_Permutaciones12 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        String str = in.nextLine();
+
+
+        HashSet<String> result = new HashSet<String>();
+        ArrayList<Character> leftChars = new ArrayList<Character>(str.chars().mapToObj(x -> (char)x).toList());
+
+        
+        Find("", leftChars, result);
+
+        for(var x : result)
+            System.out.println(x);
+    }
+
+    static void Find(String prefix, ArrayList<Character> leftChars, HashSet<String> result)
+    {
+        if(leftChars.size() == 1)
+        {
+            result.add(prefix + leftChars.get(0));
+            return;
+        }
+
+        for(int i = 0; i < leftChars.size(); ++i)
+        {
+            var newPrefix = prefix + leftChars.get(i);
+            ArrayList<Character> newLeftChars = new ArrayList<>(leftChars);
+            newLeftChars.remove(leftChars.get(i));
+
+            Find(newPrefix, newLeftChars, result);
+        }
+    }
+}
