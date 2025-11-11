ce3cb3f325d5c3473d2754a360a0c5af5f98b813
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 22:45:31 +0100

    J_Morse

---
+++ Java/src/Codewars2024/J_Morse.java
@@ -0,0 +1,73 @@
+package Codewars2024;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class J_Morse {
+
+    static HashMap<String, String> map = new HashMap<String, String>() {{
+        put(".-", "A");
+        put("-...", "B");
+        put("-.-.", "C");
+        put("-..", "D");
+        put(".", "E");
+        put("..-.", "F");
+        put("--.", "G");
+        put("....", "H");
+        put("..", "I");
+        put(".---", "J");
+        put("-.-", "K");
+        put(".-..", "L");
+        put("--", "M");
+        put("-.", "N");
+        put("---", "O");
+        put(".--.", "P");
+        put("--.-", "Q");
+        put(".--.", "P");
+        put(".-.", "R");
+        put("...", "S");
+        put("-", "T");
+        put("..--", "U");
+        put("...-", "V");
+        put(".--", "W");
+        put("-..-", "X");
+        put("-.--", "Y");
+        put("--..", "Z");
+        put("...-", "V");
+        put(".----", "1");
+        put("..---", "2");
+        put("...--", "3");
+        put("....-", "4");
+        put(".....", "5");
+        put("-....", "6");
+        put("--...", "7");
+        put("---..", "8");
+        put("----.", "9");
+        put("-----", "0");
+}};
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        var n = in.nextInt();
+        in.nextLine();
+        var strs = in.nextLine().split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (String str : strs) {
+            if(str.indexOf('~') >= 0)
+                continue;
+            
+                result.append(map.get(str));
+        }
+
+        System.out.println(result);
+    }
+}
