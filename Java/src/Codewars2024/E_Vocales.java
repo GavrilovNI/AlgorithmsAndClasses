e6cca455506744503bea2388235823c341fded70
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 23:02:24 +0100

    E_Vocales

---
+++ Java/src/Codewars2024/E_Vocales.java
@@ -0,0 +1,25 @@
+package Codewars2024;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class E_Vocales {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        var str = in.nextLine();
+
+        int result = 0;
+        for(int i = 0; i < str.length(); ++i)
+        {
+            var c = str.charAt(i);
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                result++;
+        }
+
+        System.out.println(result);
+    }
+}
