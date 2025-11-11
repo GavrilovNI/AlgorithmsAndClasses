0fc6068e96ce2f4a1d5e0bb40d2e28aca29f46b1
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 22:57:45 +0100

    G_25

---
+++ Java/src/Codewars2024/G_25.java
@@ -0,0 +1,16 @@
+package Codewars2024;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class G_25 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        in.nextLine();
+        System.out.println(25);
+    }
+}
