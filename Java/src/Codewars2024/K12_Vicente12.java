3fd904f0c4f650ea6717745c2e472f2ffb82a322
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 22:31:36 +0100

    K12_Vicente12

---
+++ Java/src/Codewars2024/K12_Vicente12.java
@@ -0,0 +1,42 @@
+package Codewars2024;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class K12_Vicente12 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        var n = in.nextInt();
+        
+        int[] values = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            values[i] = in.nextInt();
+        }
+
+        Arrays.sort(values);
+
+        long a = 0;
+        long b = 0;
+
+        for (int i = 0; i < n; i += 2)
+        {
+            a += values[i];
+            if(i + 1 < n)
+                b += values[i + 1];
+        }
+
+        if(a == b)
+            System.out.println("Empate");
+        else if(a < b)
+            System.out.println("Vicente");
+        else
+            System.out.println("Fermin");
+    }
+}
