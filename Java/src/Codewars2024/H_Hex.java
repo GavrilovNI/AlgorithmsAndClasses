fa450b5593cd003b357b76a081ca5407ae0f9682
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 22:55:52 +0100

    H_Hex

---
+++ Java/src/Codewars2024/H_Hex.java
@@ -0,0 +1,28 @@
+package Codewars2024;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class H_Hex {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        int t = in.nextInt();
+        in.nextLine();
+
+        String[] results = new String[t];
+
+        for(int j = 0; j < t; ++j)
+        {
+            var str = in.nextLine();
+
+            results[j] = str.format("rgb(%d, %d, %d)", Integer.parseInt(str.substring(1, 3), 16), Integer.parseInt(str.substring(3, 5), 16), Integer.parseInt(str.substring(5, 7), 16));
+        }
+
+        for(int j = 0; j < t; ++j)
+            System.out.println(results[j]);
+    }
+}
