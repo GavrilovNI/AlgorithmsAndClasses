16a5a86c7b72fbaa61f254b30c94e4997c42ea54
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 23:41:24 +0100

    A_Dibujo

---
+++ Java/src/Codewars2024/A_Dibujo.java
@@ -0,0 +1,26 @@
+package Codewars2024;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class A_Dibujo {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        in.nextLine();
+
+        System.out.println("###.###...\n" + //
+                        "...#......\n" + //
+                        ".#####....\n" + //
+                        "##.####..#\n" + //
+                        "#..#######\n" + //
+                        "#..#####.#\n" + //
+                        "#######...\n" + //
+                        ".#####....\n" + //
+                        "#.#.#.....\n" + //
+                        "#######...");
+    }
+}
