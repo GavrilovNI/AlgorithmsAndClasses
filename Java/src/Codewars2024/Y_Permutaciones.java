0e5d0eb67b8392183e6b77934b0b41429ccef3c3
Nikita Gavrilov <pelmeshka1110@gmail.com>
2025-11-11 23:57:24 +0100

    main -> main2

--- Java/src/Codewars2024/Y_Permutaciones.java
+++ Java/src/Codewars2024/Y_Permutaciones.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Y_Permutaciones {
 
-    public static void main(String[] args) throws Exception {
+    public static void main2(String[] args) throws Exception {
 
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
 
