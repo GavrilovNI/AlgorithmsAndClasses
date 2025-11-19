6f6d7d1d4db97eeef44aa99d22f8a783f8da5404
Nikita Gavrilov <nikitagavrilov1110@gmail.com>
2025-11-19 11:44:50 +0100

    optimize prime algorithm lol

--- Java/src/Prime.java
+++ Java/src/Prime.java
@@ -6,9 +6,11 @@ public class Prime {
             return false;
         if(value == 2)
             return true;
+        if(value % 2 == 0)
+            return false;
 
         var sqrt = Math.sqrt(value);
-        for(long i = 2; i <= sqrt; ++i)
+        for(long i = 3; i <= sqrt; i += 2)
         {
             if(value % i == 0)
                 return false;
